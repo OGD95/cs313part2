2a5fba69196d1ef50727e40c0f04aabb059d8353
Dylan Warner <dylanwarner93@gmail.com>
2021-03-20 17:47:40 -0600

    another commit

--- server.js
+++ server.js
@@ -15,12 +15,36 @@ app.get("/", function(req, res){
 app.get("/createEvent", function(req, res){
     res.sendFile('views/pages/createEvent.html', { root: __dirname});
 });
+app.get("/eventSuccesfullyCreated", createEvent);
 
 app.listen(app.get("port"), function() {
     console.log("Now listening for connections on port: ", app.get("port"));
 });
 
-function createEvent(){
+function createEvent(req, res){
+    var accountId = 1;
+    var eventTitle = req.query.eventTitle;
+    var eventDateTime = req.query.eventDate + req.query.startTime;
+    var eventEndTime = req.query.endTime;
+    var meetingLocation = req.query.meetingLocation;
+    var eventPrivacy = req.query.privacyLevel.value;
+    var eventDetails = req.query.eventDetails;
+
+    // var sql = "INSERT INTO event(accountId, eventTitle, eventDateTime, eventEndTime, meetingLocation, eventPrivacy, eventDetails) VALUES ($1, $2, $3, $4, $5, $6, $7)";
+    // var params = [accountId, eventTitle, eventDateTime, 
+    //               eventEndTime, meetingLocation, eventPrivacy, 
+    //               eventDetails];
+    // pool.query(sql, params, function(err, result){
+    //     if (err){
+    //         console.log("An error with the database occurred");
+    //         console.log(err);
+    //         callback(err, null);
+    //     }
+
+    //     console.log("Found DB result: " + JSON.stringify(result.rows));
+    //     callback(null, result.rows);
+    // })
+    res.sendFile('views/pages/creationStatus.html', { root: __dirname});
 
 }
 
